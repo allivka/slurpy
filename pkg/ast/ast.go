54ff3331429abc455ca415ae5dc50f2b04baa4e7
allivka <allivka1920@gmail.com>
2025-08-25 20:50:42 +0300

    did some stuff

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -4,51 +4,38 @@ import (
 	wp "github.com/allivka/slurpy/pkg/words"
 	tp "github.com/allivka/slurpy/pkg/tokens"
 	bts "github.com/allivka/slurpy/pkg/basic/basicTokens"
+	"errors"
 )
 
-type AstNode interface {
+type Context = interface{}
+
+type Keyword = tp.Identificator
+
+type Statement interface {
 	Execute(any) (error)
-	NewFromTokenSlice(wp.WordSlice) (AstNode, error)
-} 
+	NewFromTokenSlice(wp.WordSlice) (Statement, error)
+}
 
-type Context interface{}
-
-type Statement struct {
-	keyword tp.Identificator
+type BasicStatementCreationContext struct {
+	Keyword Keyword
 	parameters map[tp.Identificator]bts.Token
 	header bts.TokenSlice
 	body []Statement
-	defaultStatementsKeyword tp.Identificator
-	
 }
 
-func(s Statement) Execute(context any) (err error) {
-	
-	for _, statement := range s.body {
-		 err = statement.Execute(context)
-		 
-		 if err != nil {
-			return err
-		 }
+func TokensToBasicStatementContext(src bts.TokenSlice, defaultKeyword Keyword) (context BasicStatementCreationContext, err error) {
+	if src == nil || len(src) == 0 {
+		return BasicStatementCreationContext{}, errors.New("Invalid src parameter for creation of BasicStatementCreationContext for later statement creation")
 	}
 	
-	return nil
-}
-
-type Ast struct {
-	Nodes []AstNode
-}
-
-func (tree Ast) Execute(context any) (err error) {
+	var keyword Keyword
 	
-	for _, node := range tree.Nodes {
-		 err = node.Execute(context)
-		 
-		 if err != nil {
-			return err
-		 }
+	if src[0].GetWordType() != wp.Identificator {
+		keyword = defaultKeyword
+	} else if keyword, ok := src[0].(Keyword); !ok {
+		return BasicStatementCreationContext{}, errors.New("The first token of src parameter with word type Identificator cannot be represented as identificator(Something unexpected happened)")
 	}
 	
-	return nil
 	
+	return
 }
