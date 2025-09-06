489fe6c2e363e495f1113c65d9d5bb3cbaa00149
allivka <allivka1920@gmail.com>
2025-09-06 19:59:28 +0300

    fixed some stuff

--- pkg/basic/basicParser/basicParser.go
+++ pkg/basic/basicParser/basicParser.go
@@ -7,53 +7,49 @@ import (
 
 type StatementParameters = map[string]bts.TokenSlice
 
-func ParseBlockBetween(src bts.TokenSlice, startToken, endToken bts.Token) (int, bts.TokenSlice, error){ 
-	
-	if len(src) < 2 {
-		return 0, nil, fmt.Errorf("Failed parsing block of tokens '%+v' in block between %+v and %+v as provided block token slice length is less then 2 elements", src, startToken, endToken)
-	}
-	
-	if startToken == nil  || endToken == nil {
-		return 0, nil, fmt.Errorf("Border token cannot be nil")
-	}
-	
-	var (
-		blockOffset int
-		start bts.Token
-	)
-	
-	for ; start.GetWord() != startToken.GetWord(); blockOffset++ {
-		start = src[blockOffset]
-	}
-	
-	if start.GetWord() == "" {
-		return 0, nil, fmt.Errorf("Failed parsing block of tokens '%+v' in block between %+v and %+v no starting token '%+v' was found", src, startToken, endToken, startToken)
-	}
-	
-	if blockOffset >= len(src) - 1 {
-		return 0, nil, fmt.Errorf("Failed parsing block of tokens '%+v' in block between %+v and %+v no paired ending token '%+v' was found", src, startToken, endToken, endToken)
-	}
-	
-	var openedCounter = 1
-	
-	for i := blockOffset + 1; i < len(src); i++ {
-		
-		switch src[i].GetWord() {
-		case endToken.GetWord(): openedCounter--
-		case startToken.GetWord(): openedCounter++
-		}
-		
-		if openedCounter == 0 {
-			return blockOffset, src[blockOffset:i+1], nil
-		}
-		
-		if openedCounter < 0 {
-			return 0, nil, fmt.Errorf("Failed parsing block of tokens '%+v' in block between %+v and %+v no paired starting token '%+v' was found", src, startToken, endToken, startToken)
-		}
-		
-	}
-	
-	return 0, nil, fmt.Errorf("Failed parsing block of tokens '%+v' in block between %+v and %+v no paired ending token '%+v' was found", src, startToken, endToken, endToken)
+func ParseBlockBetween(src bts.TokenSlice, startToken, endToken bts.Token) (int, bts.TokenSlice, error) {
+    if len(src) < 2 {
+        return 0, nil, fmt.Errorf("failed parsing block: token slice length must be at least 2")
+    }
+    
+    if startToken == nil || endToken == nil {
+        return 0, nil, fmt.Errorf("border token cannot be nil")
+    }
+    
+    startIndex := -1
+    for i, token := range src {
+        if token.GetWord() == startToken.GetWord() {
+            startIndex = i
+            break
+        }
+    }
+    
+    if startIndex == -1 {
+        return 0, nil, fmt.Errorf("failed parsing block: start token '%s' not found", startToken.GetWord())
+    }
+    
+    openedCounter := 1
+    
+    for i := startIndex + 1; i < len(src); i++ {
+        currentWord := src[i].GetWord()
+        
+        switch currentWord {
+        case endToken.GetWord():
+            openedCounter--
+        case startToken.GetWord():
+            openedCounter++
+        }
+        
+        if openedCounter == 0 {
+            return startIndex, src[startIndex : i+1], nil
+        }
+        
+        if openedCounter < 0 {
+            return 0, nil, fmt.Errorf("failed parsing block: unbalanced tokens, too many end tokens '%s'", endToken.GetWord())
+        }
+    }
+    
+    return 0, nil, fmt.Errorf("failed parsing block: end token '%s' not found to match start token", endToken.GetWord())
 }
 
 func ParseBlockWithSeparators(src bts.TokenSlice, separatorTokens []bts.Token) (result []bts.TokenSlice, err error) {
